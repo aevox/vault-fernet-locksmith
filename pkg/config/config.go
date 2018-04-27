8e46a8697bdd8a52322b4d4bb52955d3ae776afc
Marc Fouché <marc.fouche@orange.com>
2018-04-27 16:56:42 +0200

    Remove config-file-dir option

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"flag"
 	"fmt"
-	"path/filepath"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -13,16 +12,15 @@ import (
 type Configuration struct {
 	PrimaryVault    VaultConfiguration   `mapstructure:"primary-vault"`
 	SecondaryVaults []VaultConfiguration `mapstructure:"secondary-vaults"`
-	TTL             int                  `mapstructure:"ttl"`             // Interval between each poll on vault
-	SecretPath      string               `mapstructure:"secret-path"`     // Path in vault for fernet-keys secret
-	ConfigFile      string               `mapstructure:"config-file"`     // Path to locksmith config file
-	ConfigFileDir   string               `mapstructure:"config-file-dir"` // Path to locksmith config file
-	Health          bool                 `mapstructure:"health"`          // Enable health endpoint
-	Lock            bool                 `mapstructure:"lock"`            // Use consul lock system
-	LockKey         string               `mapstructure:"lock-key"`        // What key is used for the consul lock system
-	ConsulAddress   string               `mapstructure:"consul-address"`  // Consul address
-	ConsulToken     string               `mapstructure:"consul-token"`    // Consul token use to access consul to read configuration and write lockKey
+	TTL             int                  `mapstructure:"ttl"`            // Interval between each poll on vault
+	SecretPath      string               `mapstructure:"secret-path"`    // Path in vault for fernet-keys secret
+	CfgFile         string               `mapstructure:"config"`         // Path to locksmith config file
+	Health          bool                 `mapstructure:"health"`         // Enable health endpoint
 	HealthPeriod    int                  `mapstructure:"health-period"`  // Period between each health check in seconds
+	Lock            bool                 `mapstructure:"lock"`           // Use consul lock system
+	LockKey         string               `mapstructure:"lock-key"`       // What key is used for the consul lock system
+	ConsulAddress   string               `mapstructure:"consul-address"` // Consul address
+	ConsulToken     string               `mapstructure:"consul-token"`   // Consul token use to access consul to read configuration and write lockKey
 	Version         bool                 `mapstructure:"version"`
 }
 
@@ -43,8 +41,7 @@ func DefineCmdFlags(cfg *Configuration) {
 	flag.StringVar(&cfg.PrimaryVault.Address, "vault-address", "https://127.0.0.1:8500", "Primary vault address")
 	flag.StringVar(&cfg.PrimaryVault.TokenFile, "vault-token-file", "", "File containing the vault token used to authenticate with vault")
 	flag.BoolVar(&cfg.PrimaryVault.TokenRenew, "renew-vault-token", false, "Renew vault token")
-	flag.StringVar(&cfg.ConfigFile, "config-file", "", "Name of config file (without extension)")
-	flag.StringVar(&cfg.ConfigFileDir, "config-file-dir", ".", "Path to configuration file directory")
+	flag.StringVar(&cfg.CfgFile, "config", "", "Path to config file")
 	flag.IntVar(&cfg.TTL, "ttl", 120, "Interval between each vault secret fetch")
 	flag.StringVar(&cfg.SecretPath, "secret-path", "secret/fernet-keys", "Path to the fernet-keys secret in vault")
 	flag.BoolVar(&cfg.Lock, "lock", false, "Acquires a lock with consul to ensure that only one instance of locksmith is running")
@@ -62,20 +59,19 @@ func GetConfig(cfg *Configuration) error {
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	viper.AutomaticEnv()
 
-	viper.AddConfigPath(cfg.ConfigFileDir) // directory containing config file
-	if cfg.ConfigFile != "" {
-		// Trim config file name's extension because viper does not want it
-		cfgFile := strings.TrimSuffix(cfg.ConfigFile, filepath.Ext(cfg.ConfigFile))
-		// Read configuration
-		viper.SetConfigName(cfgFile)
+	if cfg.CfgFile != "" {
+		viper.SetConfigFile(cfg.CfgFile)
+
 		err := viper.ReadInConfig()
 		if err != nil {
 			return fmt.Errorf("Error reading configuration: %v", err)
 		}
 	}
+
 	err := viper.Unmarshal(&cfg)
 	if err != nil {
 		return fmt.Errorf(" Error creating configuration struct: %v", err)
 	}
+
 	return nil
 }
